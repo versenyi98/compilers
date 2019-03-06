2e72b6cc552cfdeacbc17ea707864467069db6e0
versenyi98 <versenyi98@gmail.com>
2019-03-06 18:21:05 +0100

    Requires input, rules file, and has LL1 class

--- ll1.cpp
+++ ll1.cpp
@@ -2,125 +2,182 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
-int main(int argv, char** args) {
+class LL1 {
 
-//A táblázat betűinek kiszűrése
-	vector<pair<char, string>> rules;
-	
-	rules.push_back(make_pair('S', "aS"));
-	rules.push_back(make_pair('S', "bAc"));
-	rules.push_back(make_pair('A', "bAc"));
-	rules.push_back(make_pair('A', "d"));
+	public:
 
-	vector<char> small, both;
-	
-	for (int i = 0; i < rules.size(); i++) {
+		LL1() {};
+		~LL1() {};
 
-		bool init = false;
+		void addRule(pair<char, string> rule) {
+			rules.push_back(rule);
+		}
 
-		for (int j = 0; j < both.size(); j++) {
-			if (both[j] == rules[i].first) {
-				init = true;
-				break;
+		void fillHeaders() {
+			for (int i = 0; i < rules.size(); i++) {
+				//Szabályok bal oldáról az egyedi nagybetűk összegyűjtése
+				bool init = false;
+
+				for (int j = 0; j < headerCol.size(); j++) {
+					if (headerCol[j] == rules[i].first) {
+						init = true;
+						break;
+					}
+				}
+
+				if (!init) {
+					headerCol.push_back(rules[i].first);
+				}
+				//Szabályok jobb oldaláról az egyedi kisbetűk összegyűjtése
+				for (int j = 0; j < rules[i].second.size(); j++) {
+					bool init = false;
+
+					for (int k = 0; k < headerCol.size(); k++) {
+						if (headerCol[k] == rules[i].second[j]) {
+							init = true;
+							break;
+						}
+					}
+					if (!init && islower(rules[i].second[j])) {
+						headerCol.push_back(rules[i].second[j]);
+						headerRow.push_back(rules[i].second[j]);
+					}
+				}
+			}
+			//Rendezés, hogy nézzen ki valahogy a sorrend
+			std::sort(headerCol.begin(), headerCol.end());
+			std::sort(headerRow.begin(), headerRow.end());
+		
+			//# karakter is a táblázatban kell hogy szerepeljen
+			headerCol.push_back('#');
+			headerRow.push_back('#');
+		}
+
+		void fillTable() {
+			//Táblázat feltöltése:
+				// 0 	- error
+				// -1 - pop
+				// -2 - accept
+				// >0	-	szabály alkalmazása
+
+			for (int i = 0; i < headerCol.size(); i++) {
+				for (int j = 0; j < headerRow.size(); j++) {
+					
+					table[headerCol[i]][headerRow[j]] = 0;
+
+					for (int k = 0; k < rules.size(); k++) {
+						if (headerCol[i] == rules[k].first && headerRow[j] == rules[k].second[0]) {
+							table[headerCol[i]][headerRow[j]] = k + 1;
+						}
+					}
+
+					if (headerCol[i] == headerRow[j]) {
+						table[headerCol[i]][headerRow[j]] = -1;
+						if (headerCol[i] == '#') {
+							table[headerCol[i]][headerRow[j]] = -2;
+						}
+					} 
+				}
 			}
 		}
 
-		if (!init) {
-			both.push_back(rules[i].first);
+		void showMap() {
+			for (int i = 0; i < headerRow.size(); i++) {
+				cout << headerRow[i] << '\t';
+			}
+		 
+			cout << endl;
+
+			for (int i = 0; i < headerCol.size(); i++) {
+
+				for (int j = 0; j < headerRow.size(); j++) {
+					cout << table[headerCol[i]][headerRow[j]] << '\t';
+				}
+				cout << headerCol[i] << endl;
+			}
+			cout << endl;
 		}
 
-		for (int j = 0; j < rules[i].second.size(); j++) {
-			bool init = false;
+		void parse(string input) {
+		//Elemzés
+			input += "#";
+			string state = "S#";
+			vector<int> rulesUsed;
 
-			for (int k = 0; k < both.size(); k++) {
-				if (both[k] == rules[i].second[j]) {
-					init = true;
+			while (true) {
+
+				cout << input << ",\t" << state << ",\t(";
+				for (int i = 0; i < rulesUsed.size(); i++) {
+					cout << rulesUsed[i] << ' ';
+				}
+				cout << ")\n";
+
+				int number = table[state[0]][input[0]];
+
+				if (number > 0) {
+					state = rules[number - 1].second + state.substr(1);
+					rulesUsed.push_back(number);
+				} else if (number == -1) {
+					input = input.substr(1);
+					state = state.substr(1);
+				} else if (number == -2) {
+					cout << "A szó eleme a nyelvnek..." << endl;
+					break;
+				} else {
+					cout << "Error" << endl;
 					break;
 				}
 			}
-			if (!init && islower(rules[i].second[j])) {
-				both.push_back(rules[i].second[j]);
-				small.push_back(rules[i].second[j]);
-			}
 		}
+
+	private:
+		vector<pair<char, string>> rules;
+		map<char, map<char, int>> table;
+		vector<char> headerRow, headerCol;
+};
+
+int main(int argv, char** args) {
+
+	ifstream fin;
+
+	if(argv > 1) {
+		fin.open(args[1]);
 	}
 
-	std::sort(both.begin(), both.end());
-	std::sort(small.begin(), small.end());
-
-	both.push_back('#');
-	small.push_back('#');
-
-//Map feltöltése
-	map<char, map<char, int>> tableMap;
-
-
-	for (int i = 0; i < both.size(); i++) {
-		for (int j = 0; j < small.size(); j++) {
-			
-			tableMap[both[i]][small[j]] = 0;
-
-			for (int k = 0; k < rules.size(); k++) {
-				if (both[i] == rules[k].first && small[j] == rules[k].second[0]) {
-					tableMap[both[i]][small[j]] = k + 1;
-				}
-			}
-
-			if (both[i] == small[j]) {
-
-				tableMap[both[i]][small[j]] = -1;
-
-				if (both[i] == '#') {
-					tableMap[both[i]][small[j]] = -2;
-				}
-			} 
-		}
+	if(!fin.is_open()) {
+		cerr << "Használat: ./ll1 rulesLL1.txt" << endl;
+		return -1;
 	}
 
-//Map kirajzolása
-	for (int i = 0; i < small.size(); i++) {
-		cout << small[i] << '\t';
+	LL1 ll1;
+
+	string temp;
+	while(getline(fin, temp)) {
+		ll1.addRule(make_pair(temp[0], temp.substr(2)));
 	}
- 
-	cout << endl;
 
-	for (int i = 0; i < both.size(); i++) {
+	ll1.fillHeaders();
+	ll1.fillTable();
+	ll1.showMap();
 
-		for (int j = 0; j < small.size(); j++) {
-			cout << tableMap[both[i]][small[j]] << '\t';
-		}
-		cout << both[i] << endl;
-	}
-	cout << endl;
+	string input;
 
-//Elemzés
-	string input = "aabbdcc#";
-	string allapot = "S#";
+	cout << "Adjon meg egy inputot: ";
 
-	while (true) {
+	while (getline(cin, input)) {
 
-		cout << "Input: " << input << "\tÁllapot: " << allapot << endl;
-
-		int number = tableMap[allapot[0]][input[0]];
-
-		if (number > 0) {
-			allapot = rules[number - 1].second + allapot.substr(1);	
-		} else if (number == -1) {
-			input = input.substr(1);
-			allapot = allapot.substr(1);
-		} else if (number == -2) {
-			cout << "A szó eleme a nyelvnek..." << endl;
-			break;
-		} else {
-			cout << "Error" << endl;
+		if (input.length() == 0) {
 			break;
 		}
+		cout << endl;
+		ll1.parse(input);
 
+		cout << endl << "Adjon meg egy inputot: ";
 	}
 
 	return 0;
 }
-
